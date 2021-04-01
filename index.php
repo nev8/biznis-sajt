1a0d3aea8d2f75c251b812a4959e637172d0dcc4
Nevena <nevenablagojevic95@gmail.com>
2021-04-01 16:30:46 +0200

    business css

--- index.php
+++ index.php
@@ -1,84 +1,132 @@
 <?php include "partials/header.php" ?>
 
-    <!--Header-->
-    <header> 
-        <h1>Trusted and <br> professionals advisers</h1>
-        <h3>for your business</h3>
-        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
-        <button>About us</button>
-    </header>
+<!--Header-->
+<header class="mainHeader">
+    <h1>Trusted and <br> professionals advisers</h1>
+    <h3>for your business</h3>
+    <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+    <button>About us</button>
+</header>
 
-    <!--Headings-->
-    <section class="container headings">
-        <article>
-            <h2>01</h2>
-        </article>
-        <article>
-            <h3>about us</h3>
-            <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
-            <h4>Get your Business in 4 easy steps</h4>
-        </article>
-    </section>
+<!--Headings-->
+<section class="container headings">
+    <article>
+        <h2>01</h2>
+    </article>
+    <article>
+        <h3>about us</h3>
+        <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </article>
+</section>
 
-    <!--About-->
-    <section class="about">
-        <article> <img src="img/about.jpg" alt=""></article>
-        <article>
-            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>  
-        </article>
-    </section>
+<!--About-->
+<section class="about">
+    <article> <img src="img/about.jpg" alt=""></article>
+    <article>
+        <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste
+            voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit
+            molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio
+            repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>
+    </article>
+</section>
 
-    <!--Headings-->
-    <section class="container headings">
+<!--Headings-->
+<section class="container headings">
+    <article>
+        <h2>02</h2>
+    </article>
+    <article>
+        <h3>services</h3>
+        <h2><span>how to provide</span> tools that help anyone</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </article>
+</section>
+
+<!--Services-->
+<section class="services container">
+    <article>
+        <i class="far fa-chart-bar"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+    <article>
+        <i class="fas fa-bullseye"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+    <article>
+        <i class="fas fa-map-signs"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+    <article>
+        <i class="fas fa-filter"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+    <article>
+        <i class="fas fa-hand-holding-usd"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+    <article>
+        <i class="fas fa-bullhorn"></i>
+        <h3>Marketing</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur
+            omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.
+        </p>
+    </article>
+</section>
+
+<!--Business-->
+<section class="business">
+    <header class="container headings black">
         <article>
-            <h2>02</h2>
+            <h2>03</h2>
         </article>
         <article>
             <h3>services</h3>
             <h2><span>how to provide</span> tools that help anyone</h2>
             <h4>Get your Business in 4 easy steps</h4>
+            <button>About us</button>
         </article>
-    </section>
+    </header>
+    <div class="counter conatiner">
+        <div class="col">
+            <h2>18</h2>
+            <h4>Nominations</h4>
+            <hr>
+        </div>
+        <div class="col">
+            <h2>5</h2>
+            <h4>Awards</h4>
+            <hr>
+        </div>
+        <div class="col">
+            <h2>45</h2>
+            <h4>Agencies</h4>
+            <hr>
+        </div>
+    </div>
+</section>
 
-    <!--Services-->
-    <section class="services container">
-        <article>
-            <i class="far fa-chart-bar"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-        <article>
-            <i class="fas fa-bullseye"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-        <article>
-            <i class="fas fa-map-sign"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-        <article>
-            <i class="fas fa-filter"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-        <article>
-            <i class="fas fa-hand-holding-usd"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-        <article>
-            <i class="fas fa-bullhorn"></i>
-            <h3>Marketing</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
-        </article>
-    </section>
 
-    <?php include "partials/footer.php" ?> 
+<?php include "partials/footer.php" ?>
