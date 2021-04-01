c5d8983fce6ff958b5e9eb2feca22243aa41177c
Nevena <nevenablagojevic95@gmail.com>
2021-04-01 15:05:11 +0200

    about html

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <button>About us</button>
     </header>
 
-    <!--Headings-->\
+    <!--Headings-->
     <section class="container headings">
         <article>
             <h2>01</h2>
@@ -17,6 +17,15 @@
             <h3>about us</h3>
             <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
             <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+    <!--About-->
+    <section class="about">
+        <article> <img src="img/about.jpg" alt=""></article>
+        <article>
+            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>  
         </article>
     </section>
 
