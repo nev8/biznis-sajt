61822b4eee65a9d513df2a9202d65e1f1d8af32d
Nevena <nevenablagojevic95@gmail.com>
2021-04-01 10:57:10 +0200

    footer css

--- index.php
+++ index.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include "partials/header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="scss/style.css">
-</head>
+    <h1>Ovde sutra content</h1>
 
-<body>
-    <!--Nav-->
-    <nav>
-        <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Team</a></li>
-            <li><a href="">Sign up</a></li>
-            <li><a href=""><i class="fas fa-search"></i></a></li>
-        </ul>
-    </nav>
-
-    <!--Footer-->
-    <footer>
-        <div class="container">
-            <div class="col">
-                <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-            </div>
-            <div class="col">
-                <i class="fas fa-map-marker-alt"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-            <div class="col">
-                <i class="fas fa-envelope"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-            <div class="col">
-                <i class="fas fa-phone"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-        </div>
-    </footer>
-</body>
-
-</html>
+    <?php include "partials/footer.php" ?>
